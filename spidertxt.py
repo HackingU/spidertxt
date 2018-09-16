d326e1d42f677397196fbb6afba1f37279543ebe
IgorAssuncao <igor.ormonde@gmail.com>
2018-09-15 22:02:38 -0300

    added requests to each item in robots.txt

--- spidertxt.py
+++ spidertxt.py
@@ -7,7 +7,7 @@ def connect(url):
     if url[-1] != '/':
       url += '/'
     req = requests.get(url + 'robots.txt')
-    if req.status_code == 200:
+    if req.status_code == 200: 
       return req.text
     else:
       exit('robots.txt unavailable')
@@ -15,12 +15,29 @@ def connect(url):
     print('Error: %s' % e)
     exit(1)
 
+def tryRobotsLines(robots, url):
+  if url[-1] != '/':
+    url += '/'
+  for item in robots:
+    if item[-1] != '/':
+      item += '/'
+    if item.startswith('Disallow'):
+      firstIndex = int(item.index('/'))
+      lastIndex = int(item.rindex('/'))
+      item = item[firstIndex+1:lastIndex]
+      req = requests.get(url + item)
+      print(f'{url + item} - {req.status_code}')
+ 
+
 def getRobots(request):
   if request:
     robots = [line for line in filter(None, request.split("\n"))]
+    print(robots)
     return robots
   else:
     exit('robots.txt unavailable')
+
 if __name__ == '__main__':
   robots = getRobots(connect('https://hackingu.net'))
-  print(robots)
+  print()
+  tryRobotsLines(robots, 'https://hackingu.net')
