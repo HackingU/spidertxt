772c87fdd8705a98b08848da982850a23b037c3f
IgorAssuncao <igor.ormonde@gmail.com>
2019-01-06 21:19:16 -0200

    removed clear function and converted indentation to spaces

--- spidertxt.py
+++ spidertxt.py
@@ -4,88 +4,90 @@ import requests
 import sys
 from os import system, name
 
-def clear():
-	if name == 'nt':
-		system('cls')
-	else:
-		system('clear')
 
 def getUrl(args):
-	if len(args) > 1:
-		return args[1]
-	else:
-		exit('No URL found.\nPlease specify a valid URL.')
+    if len(args) > 1:
+        return args[1]
+    else:
+        exit('No URL found.\nPlease specify a valid URL.')
+
 
 def fixUrl(url):
-	if url[-1] != '/':
-		url += '/'
-	if not url.startswith('http://') and url.startswith('https://'):
-		url = url.replace('https://', '')
-		url = 'http://' + url
-	return url
+    if url[-1] != '/':
+        url += '/'
+    if not url.startswith('http://') and url.startswith('https://'):
+        url = url.replace('https://', '')
+        url = 'http://' + url
+    return url
+
 
 def connect(url):
-	try:
-		req = requests.get(url + 'robots.txt')
-		if req.status_code == 200:
-			return req.text
-		else:
-			exit('robots.txt unavailable')
-	except requests.exceptions.RequestException as e:
-		print(f'Error: {e}')
-		exit(1)
+    try:
+        req = requests.get(url + 'robots.txt')
+        if req.status_code == 200:
+            return req.text
+        else:
+            exit('robots.txt unavailable')
+    except requests.exceptions.RequestException as e:
+        print(f'Error: {e}')
+        exit(1)
+
 
 def tryRobotsLines(robots, url):
-	for robot in robots:
-		if robot[-1] != '/':
-			robot += '/'
-		if robot.startswith('Disallow'):
-			firstIndex = int(robot.index('/'))
-			lastIndex = int(robot.rindex('/'))
-			robot = robot[firstIndex+1:lastIndex]
-			req = requests.get(url + robot)
-			print(f'Item: {url + robot} - Status code: {req.status_code}')
+    for robot in robots:
+        if robot[-1] != '/':
+            robot += '/'
+        if robot.startswith('Disallow'):
+            firstIndex = int(robot.index('/'))
+            lastIndex = int(robot.rindex('/'))
+            robot = robot[firstIndex+1:lastIndex]
+            req = requests.get(url + robot)
+            print(f'Item: {url + robot} - Status code: {req.status_code}')
 
-			# Not necessary to show history because can't fix http/https bug
-			# if (req.history) and (req.history[0].status_code == 301 or req.history[0].status_code == 302):
-			# 	print(f"Redirected from: {req.history[0].headers['Location']} - Status code: {req.history[0].status_code}")
-			content = req.text
-			title = everything_between(content,'<title>','</title>')
-			if title.startswith('Index of '):
-				listIndexOfContent(url + robot)
+            # Not necessary to show history because can't fix http/https bug
+            # if (req.history) and (req.history[0].status_code == 301 or req.history[0].status_code == 302):
+            # 	print(f"Redirected from: {req.history[0].headers['Location']} - Status code: {req.history[0].status_code}")
+            content = req.text
+            title = everything_between(content, '<title>', '</title>')
+            if title.startswith('Index of '):
+                listIndexOfContent(url + robot)
+
 
 def getRobots(request):
-	if request:
-		robots = [line for line in filter(None, request.split("\n"))]
-		print(f'{robots}\n')
-		return robots
-	else:
-		print('robots.txt unavailable')
+    if request:
+        robots = [line for line in filter(None, request.split("\n"))]
+        print(f'{robots}\n')
+        return robots
+    else:
+        print('robots.txt unavailable')
+
 
 def everything_between(content, begin, end):
-    idx1=content.find(begin)
-    idx2=content.find(end,idx1)
+    idx1 = content.find(begin)
+    idx2 = content.find(end, idx1)
     return content[idx1+len(begin):idx2].strip()
 
+
 def listIndexOfContent(url):
-	req = requests.get(url)
-	req_text = req.text
-	tag_begin = '<a href="'
-	tag_end = '">'
-	req_text_lines = req_text.split('\n')
-	for line in req_text_lines:
-		line = line.strip()
-		if tag_begin in line:
-			item = everything_between(line,tag_begin,tag_end)
-			print(f'{item}')
+    req = requests.get(url)
+    req_text = req.text
+    tag_begin = '<a href="'
+    tag_end = '">'
+    req_text_lines = req_text.split('\n')
+    for line in req_text_lines:
+        line = line.strip()
+        if tag_begin in line:
+            item = everything_between(line, tag_begin, tag_end)
+            print(f'{item}')
+
 
 def main():
-	clear()
-	url = getUrl(sys.argv)
-	url = fixUrl(url)
-	print(f'URL: {url}\n')
-	robots = getRobots(connect(url))
-	tryRobotsLines(robots, url)
+    url = getUrl(sys.argv)
+    url = fixUrl(url)
+    print(f'URL: {url}\n')
+    robots = getRobots(connect(url))
+    tryRobotsLines(robots, url)
+
 
 if __name__ == '__main__':
-	main()
+    main()
